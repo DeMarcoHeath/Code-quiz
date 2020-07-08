9d9ff52c8a82315fed622976851a2f6ab6dedccd
Demarco Heath <demarcoheath@gmail.com>
2020-07-07 22:46:27 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+var jsQuestions = [
+    {
+      title: "Who was Nat Turner:",
+      choices: ["slave master", "slave", "scientist", "Revolutionary"],
+      answer: "Revolutionary"
+    },
+    {
+      title: "Malcolm X was the National Spokesperson for what organization?",
+      choices: ["Nation Of Islam", "NAACP", "Boys and Girls Club"],
+      answer: "Nation of Islam"
+    },
+    {
+      title: "What seafaring African Nation reached the Americas pre columbus?",
+      choices: ["Aztecs", "Egyptians", "Phonecians"],
+      answer: "Phonecians"
+    },
+    {
+      title: "Who is the founder of the Black Guerilla Family?",
+      choices: ["Clint Eastwood", "Bill Cosby", "My Mama", "George Jackson"],
+      answer: "George Jackson"
+    },
+    {
+      title: "Bill Clinton is Black",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Black People do not like chicken",
+      choices: ["True", "False"],
+      answer: "False"
+      
+    }
+  ];
+  
+  var foodQuestions = [
+    
+  
+    {
+      title: "A tomato is a fruit?",
+      choices: ["true", "false"],
+      answer: "true"
+    },
+    {
+      title: "What is the base vegetable of a pickle?",
+      choices: ["cucumber", "watermelon", "onion"],
+      answer: "cucumber"
+    },
+    {
+      title: "Which is Tastiest?",
+      choices: ["chitterlings", "pig feet", "liver", "steak"],
+      answer: "chitterlings"
+    },
+    {
+      title: "Which pratice is NOT consider safe food practice",
+      choices: ["Store meat in fridge", "Store meat in sun on front porch", "Eat chocolate", "Let me cook it"],
+      answer: "let me cook it"
+    }
+  ]
